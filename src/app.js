196d73984dfc0128e860040de36759accf855abf
igorevanss <igorevans.pro@gmail.com>
2023-04-14 17:58:34 -0300

    criado a conexão com Banco de Dados e configurado o joi

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import express from "express"
-import cors from "cors"
-import { MongoClient } from "mongodb"
-import dotenv from "dotenv"
+import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+import joi from 'joi'
+import dayjs from 'dayjs'
 
 const app = express()
 
@@ -9,11 +11,28 @@ app.use(express.json())
 app.use(cors())
 dotenv.config()
 
-let db
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
+let db
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db()
+  })
+  .catch(err => console.log(err.message))
+
+const participantRules = joi.object({
+  name: joi.string().min(1).required()
+})
+
+const messageRules = joi.object({
+  from: joi.string().required(),
+  to: joi.string().min(1).required(),
+  text: joi.string().min(1).required(),
+  type: joi.string().valid('message', 'private_message').required(),
+  time: joi.string()
+})
+
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
