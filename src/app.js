2538b863facb67b57b8e9b1fcf8cb5c12eb54e32
igorevanss <igorevans.pro@gmail.com>
2023-04-17 11:13:34 -0300

    Post de participants concluído

--- src/app.js
+++ src/app.js
@@ -32,6 +32,48 @@ const messageRules = joi.object({
   time: joi.string()
 })
 
+app.post('/participants', async (req, res) => {
+  const participant = req.body
+
+  const validation = participantRules.validate(participant, {
+    abortEarly: false
+  })
+
+  if (validation.error) {
+    const errors = validation.error.details.map(detail => detail.message)
+    res.status(422).send(errors)
+    return
+  }
+
+  try {
+    const participantExists = await db
+      .collection('participants')
+      .findOne({ name: participant.name })
+
+    if (participantExists) {
+      res.sendStatus(409)
+      return
+    }
+
+    await db.collection('participants').insertOne({
+      name: participant.name,
+      lastStatus: Date.now()
+    })
+
+    await db.collection('messages').insertOne({
+      from: participant.name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss')
+    })
+
+    res.send(201)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
 
 
 const PORT = 5000
